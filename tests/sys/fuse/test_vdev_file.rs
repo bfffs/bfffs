362cca25cfd23a88168e0b0dd3706a476f909267
Alan Somers <asomers@gmail.com>
2017-10-11 23:09:03 -0600

    Add more tests for VdevFile

--- tests/sys/fuse/test_vdev_file.rs
+++ tests/sys/fuse/test_vdev_file.rs
@@ -8,11 +8,11 @@ macro_rules! t {
 test_suite! {
     name vdev_file_tests;
 
-    use arkfs::common::vdev::Vdev;
+    use arkfs::common::vdev::{SGVdev, Vdev};
     use arkfs::sys::vdev_file::*;
     use futures::Future;
     use std::fs;
-    use std::io::Read;
+    use std::io::{Read, Seek, SeekFrom, Write};
     use std::ops::Deref;
     use std::path::PathBuf;
     use std::rc::Rc;
@@ -49,6 +49,49 @@ test_suite! {
         assert_eq!(vdev.val.1.start_of_zone(1), 1 << 19);
     }
 
+    test read_at() {
+        // Create the initial file
+        let dir = t!(TempDir::new("test_read_at"));
+        let path = dir.path().join("vdev");
+        let wbuf = vec![42u8; 4096];
+        {
+            let mut f = t!(fs::File::create(&path));
+            t!(f.write_all(wbuf.as_slice()));
+            t!(f.set_len(1 << 26));
+        }
+
+        // Run the test
+        let rbuf = Rc::new(vec![0u8; 4096].into_boxed_slice());
+        let mut core = Core::new().unwrap();
+        let vdev = VdevFile::open(path, core.handle());
+        let fut = vdev.read_at(rbuf.clone(), 0);
+        assert_eq!(4096, core.run(fut).unwrap());
+        assert_eq!(rbuf.deref().deref(), wbuf.as_slice());
+    }
+
+    test readv_at() {
+        // Create the initial file
+        let dir = t!(TempDir::new("test_readv_at"));
+        let path = dir.path().join("vdev");
+        let wbuf = vec![42u8; 4096];
+        {
+            let mut f = t!(fs::File::create(&path));
+            t!(f.write_all(wbuf.as_slice()));
+            t!(f.set_len(1 << 26));
+        }
+
+        // Run the test
+        let rbuf0 = Rc::new(vec![0u8; 1024].into_boxed_slice());
+        let rbuf1 = Rc::new(vec![0u8; 3072].into_boxed_slice());
+        let rbufs = vec![rbuf0.clone(), rbuf1.clone()].into_boxed_slice();
+        let mut core = Core::new().unwrap();
+        let vdev = VdevFile::open(path, core.handle());
+        let fut = vdev.readv_at(rbufs, 0);
+        assert_eq!(4096, core.run(fut).unwrap());
+        assert_eq!(rbuf0.deref().deref(), &wbuf[0..1024]);
+        assert_eq!(rbuf1.deref().deref(), &wbuf[1024..4096]);
+    }
+
     test write_at(vdev) {
         let wbuf = Rc::new(vec![42u8; 4096].into_boxed_slice());
         let mut rbuf = vec![0u8; 4096];
@@ -57,6 +100,30 @@ test_suite! {
         let mut f = t!(fs::File::open(vdev.val.2));
         t!(f.read_exact(&mut rbuf));
         assert_eq!(rbuf, wbuf.deref().deref());
+    }
+
+    test write_at_lba(vdev) {
+        let wbuf = Rc::new(vec![42u8; 4096].into_boxed_slice());
+        let mut rbuf = vec![0u8; 4096];
+        let fut = vdev.val.1.write_at(wbuf.clone(), 1);
+        assert_eq!(4096, vdev.val.0.run(fut).unwrap());
+        let mut f = t!(fs::File::open(vdev.val.2));
+        t!(f.seek(SeekFrom::Start(4096)));
+        t!(f.read_exact(&mut rbuf));
+        assert_eq!(rbuf, wbuf.deref().deref());
+    }
+
+    test writev_at(vdev) {
+        let wbuf0 = Rc::new(vec![21u8; 1024].into_boxed_slice());
+        let wbuf1 = Rc::new(vec![42u8; 3072].into_boxed_slice());
+        let wbufs = vec![wbuf0.clone(), wbuf1.clone()].into_boxed_slice();
+        let mut rbuf = vec![0u8; 4096];
+        let fut = vdev.val.1.writev_at(wbufs, 0);
+        assert_eq!(4096, vdev.val.0.run(fut).unwrap());
+        let mut f = t!(fs::File::open(vdev.val.2));
+        t!(f.read_exact(&mut rbuf));
+        assert_eq!(&rbuf[0..1024], wbuf0.deref().deref());
+        assert_eq!(&rbuf[1024..4096], wbuf1.deref().deref());
     }
 
     test read_after_write(vdev) {
