f8105f90178df637b1408d4b74da1401b528642e
Alan Somers <asomers@gmail.com>
2017-10-29 16:10:24 -0600

    Add a benchmark for PrimeS::id2loc

---
+++ benches/primes.rs
@@ -0,0 +1,22 @@
+#![feature(test)]
+
+extern crate arkfs;
+extern crate test;
+
+use arkfs::common::declust::*;
+use arkfs::common::prime_s::*;
+use test::Bencher;
+
+/// Benchmark the speed of PrimeS::id2loc using a largeish layout
+#[bench]
+fn bench_id2loc(bench: &mut Bencher) {
+    let n = 23;
+    let k = 19;
+    let f = 3;
+
+    let locator = PrimeS::new(n, k, f);
+
+    bench.iter(move || {
+        locator.id2loc(ChunkId::Parity(1234567, 1))
+    });
+}
