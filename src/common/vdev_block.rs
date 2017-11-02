b0b630a92cf46ec6582986b0ec1e01ccd01466df
Alan Somers <asomers@gmail.com>
2017-11-02 09:44:49 -0600

    Move VdevBlock's mockers-based tests into the source file
    Just for consistency with how VdevRaid does it.

--- src/common/vdev_block.rs
+++ src/common/vdev_block.rs
@@ -380,3 +380,143 @@ impl Vdev for VdevBlock {
         Box::new(VdevBlockFut::new(receiver))
     }
 }
+
+#[cfg(feature = "mocks")]
+#[cfg(test)]
+test_suite! {
+    name mock_vdev_block;
+
+    use super::*;
+    use futures::future;
+    use mockers::{Scenario, Sequence};
+    use mockers::matchers::ANY;
+    use std::io::Error;
+    use std::rc::Rc;
+    use tokio_core::reactor::{Core, Handle};
+
+    mock!{
+        MockVdevLeaf2,
+        vdev,
+        trait Vdev {
+            fn handle(&self) -> Handle;
+            fn lba2zone(&self, lba: LbaT) -> ZoneT;
+            fn read_at(&self, buf: IoVec, lba: LbaT) -> Box<VdevFut>;
+            fn size(&self) -> LbaT;
+            fn start_of_zone(&self, zone: ZoneT) -> LbaT;
+            fn write_at(&self, buf: IoVec, lba: LbaT) -> Box<VdevFut>;
+        },
+        vdev,
+        trait SGVdev  {
+            fn readv_at(&self, bufs: SGList, lba: LbaT) -> Box<VdevFut>;
+            fn writev_at(&self, bufs: SGList, lba: LbaT) -> Box<VdevFut>;
+        },
+        vdev_leaf,
+        trait VdevLeaf  {
+        }
+    }
+
+    fixture!( mocks() -> (Scenario, Box<MockVdevLeaf2>) {
+            setup(&mut self) {
+            let scenario = Scenario::new();
+            let leaf = Box::new(scenario.create_mock::<MockVdevLeaf2>());
+            scenario.expect(leaf.size_call()
+                                .and_return(16384));
+            scenario.expect(leaf.lba2zone_call(ANY)
+                                .and_return_clone(0)
+                                .times(..));
+            scenario.expect(leaf.start_of_zone_call(0)
+                                .and_return_clone(0)
+                                .times(..));
+            (scenario, leaf)
+        }
+    });
+
+    // Reads should be passed straight through, even if they're out-of-order
+    test read_at(mocks) {
+        let scenario = mocks.val.0;
+        let leaf = mocks.val.1;
+        let mut seq = Sequence::new();
+        seq.expect(leaf.read_at_call(ANY, 1)
+                        .and_return(Box::new(future::ok::<isize, Error>((0)))));
+        seq.expect(leaf.read_at_call(ANY, 0)
+                        .and_return(Box::new(future::ok::<isize, Error>((0)))));
+        scenario.expect(seq);
+
+        let rbuf = Rc::new(vec![0u8; 4096].into_boxed_slice());
+        let mut core = Core::new().unwrap();
+        let vdev = Rc::new(VdevBlock::open(leaf, core.handle()));
+        let first = vdev.read_at(rbuf.clone(), 1);
+        let second = vdev.read_at(rbuf.clone(), 0);
+        let futs = future::Future::join(first, second);
+        core.run(futs).unwrap();
+    }
+
+    // Basic writing at the WP works
+    test write_at_0(mocks) {
+        let scenario = mocks.val.0;
+        let leaf = mocks.val.1;
+        scenario.expect(leaf.write_at_call(ANY, 0)
+                        .and_return(Box::new(future::ok::<isize, Error>((0)))));
+
+        let wbuf = Rc::new(vec![0u8; 4096].into_boxed_slice());
+        let mut core = Core::new().unwrap();
+        let vdev = Rc::new(VdevBlock::open(leaf, core.handle()));
+        let fut = vdev.write_at(wbuf.clone(), 0);
+        core.run(fut).unwrap();
+    }
+
+    // Basic vectored writing at the WP works
+    test writev_at_0(mocks) {
+        let scenario = mocks.val.0;
+        let leaf = mocks.val.1;
+        scenario.expect(leaf.writev_at_call(ANY, 0)
+                        .and_return(Box::new(future::ok::<isize, Error>((0)))));
+
+        let wbuf0 = Rc::new(vec![0u8; 1024].into_boxed_slice());
+        let wbuf1 = Rc::new(vec![0u8; 3072].into_boxed_slice());
+        let wbufs = vec![wbuf0, wbuf1].into_boxed_slice();
+        let mut core = Core::new().unwrap();
+        let vdev = Rc::new(VdevBlock::open(leaf, core.handle()));
+        let fut = vdev.writev_at(wbufs, 0);
+        core.run(fut).unwrap();
+    }
+
+    // Writes should be reordered and combined if out-of-order
+    test write_at_combining(mocks) {
+        let scenario = mocks.val.0;
+        let leaf = mocks.val.1;
+        scenario.expect(leaf.writev_at_call(ANY, 0)
+                        .and_return(Box::new(future::ok::<isize, Error>((0)))));
+
+        let wbuf = Rc::new(vec![0u8; 4096].into_boxed_slice());
+        let mut core = Core::new().unwrap();
+        let vdev = Rc::new(VdevBlock::open(leaf, core.handle()));
+        // Issue writes out-of-order
+        let first = vdev.write_at(wbuf.clone(), 1);
+        let second = vdev.write_at(wbuf.clone(), 0);
+        let futs = future::Future::join(first, second);
+        core.run(futs).unwrap();
+    }
+
+    // Writes should be issued ASAP, even if they could be combined later
+    test write_at_issue_asap(mocks) {
+        let scenario = mocks.val.0;
+        let leaf = mocks.val.1;
+        let mut seq = Sequence::new();
+        scenario.expect(leaf.lba2zone_call(2).and_return(0));
+        seq.expect(leaf.writev_at_call(ANY, 0)
+                        .and_return(Box::new(future::ok::<isize, Error>((0)))));
+        seq.expect(leaf.write_at_call(ANY, 2)
+                        .and_return(Box::new(future::ok::<isize, Error>((0)))));
+        scenario.expect(seq);
+
+        let wbuf = Rc::new(vec![0u8; 4096].into_boxed_slice());
+        let mut core = Core::new().unwrap();
+        let vdev = Rc::new(VdevBlock::open(leaf, core.handle()));
+        let first = vdev.write_at(wbuf.clone(), 1);
+        let second = vdev.write_at(wbuf.clone(), 0);
+        let third = vdev.write_at(wbuf.clone(), 2);
+        let futs = future::Future::join3(first, second, third);
+        core.run(futs).unwrap();
+    }
+}
--- tests/mod.rs
+++ tests/mod.rs
@@ -1,13 +1,9 @@
-#![cfg_attr(feature = "mocks", feature(plugin))]
-#![cfg_attr(feature = "mocks", plugin(mockers_macros))]
-
 extern crate arkfs;
 extern crate fixedbitset;
 extern crate futures;
 #[macro_use] extern crate galvanic_test;
 extern crate itertools;
 extern crate rand;
-#[cfg(feature = "mocks")] extern crate mockers;
 extern crate tempdir;
 extern crate tokio_core;
 extern crate tokio_file;
