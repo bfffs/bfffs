fc826357414f6758dcd45e77918d70be2b04bffe
Alan Somers <asomers@gmail.com>
2018-02-26 10:09:54 -0700

    Upgrade mockers to 0.9.4
    Finally, mockers works with the latest rust-nightly!

--- test.sh
+++ test.sh
@@ -3,12 +3,12 @@ set -e
 
 # Run all of the tests in order of most likely to fail to least likely
 
-# Mockers only works with this specific toolchain
-rustup run nightly-2018-02-04-x86_64-unknown-freebsd cargo test --all-features
+# Mockers is very picky about rust versions.  Mockers 0.9.4 is known to work
+# with rust-nightly 1.26.0 as of Feb-26-2018.
+rustup run nightly cargo test --all-features
 
 # Check that benchmarks run, but don't care about results
 rustup run nightly cargo bench
 
-# It should also work on stable and recent nightly
+# It should also work on stable
 rustup run stable cargo test -j1
-rustup run nightly cargo test
