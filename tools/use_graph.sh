3d175e1aabf1951af8d9bd340933070c5cf8b5e1
Alan Somers <asomers@gmail.com>
2018-04-08 09:44:32 -0600

    use_graph.sh: update external crate list

--- tools/use_graph.sh
+++ tools/use_graph.sh
@@ -10,7 +10,7 @@
 # excluded from the graph.  Then run the script.  "use_graph.svg" will be
 # created in PWD.
 
-EXTERNAL_CRATES='(std|divbuf|fixedbitset|futures|isa_l|itertools|mio|mockers|modulo|permutohedron|nix|rand|super|tempdir|tokio|tokio_file)'
+EXTERNAL_CRATES='(std|divbuf|fixedbitset|futures|isa_l|itertools|mio|mockers|modulo|permutohedron|nix|rand|serde|serde_cbor|super|tempdir|tokio|tokio_file|uuid)'
 BORING_FILES='^\<(common)\>$'
 
 EXTERNAL_PAT="^use.${EXTERNAL_CRATES}"
