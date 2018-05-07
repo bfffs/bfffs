b82ff8d110794afe5b27132fff58cde106432058
Alan Somers <asomers@gmail.com>
2018-05-07 10:32:30 -0600

    test.sh: run tests in parallel
    This has worked fine ever since I redid the scheduler.

--- test.sh
+++ test.sh
@@ -5,10 +5,10 @@ set -e
 
 # Mockers is very picky about rust versions.  Mockers 0.9.4 is known to work
 # with rust-nightly 1.26.0 as of Feb-26-2018.
-cargo +nightly test --all-features -- --test-threads=1
+cargo +nightly test --all-features
 
 # Check that benchmarks run, but don't care about results
 rustup run nightly cargo bench
 
 # It should also work on stable
-cargo +nightly test -j1 -- --test-threads=1
+cargo +nightly test -j1
