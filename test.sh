e65372e85fed675e2870dcc13f856f2da3776bee
Alan Somers <asomers@gmail.com>
2018-05-11 12:02:10 -0600

    test.sh: specify exactly which nightly compiler to use
    mockers is very picky

--- test.sh
+++ test.sh
@@ -5,10 +5,10 @@ set -e
 
 # Mockers is very picky about rust versions.  Mockers 0.9.4 is known to work
 # with rust-nightly 1.26.0 as of Feb-26-2018.
-cargo +nightly test --all-features
+cargo +nightly-2018-02-25-x86_64-unknown-freebsd test --all-features
 
 # Check that benchmarks run, but don't care about results
-rustup run nightly cargo bench
+rustup run nightly-2018-02-25-x86_64-unknown-freebsd cargo bench
 
 # It should also work on stable
 cargo test -j1
