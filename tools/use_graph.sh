873f7a95f02754db066ede4d82103a7bad7562d5
Alan Somers <asomers@gmail.com>
2018-06-15 21:09:48 -0600

    use_graph.sh: use cargo-modules
    This script hasn't worked since I switched to nested use statements.  The
    cargo-modules crate does a better job, too.

--- tools/use_graph.sh
+++ tools/use_graph.sh
@@ -3,71 +3,25 @@
 # Render a graph of the use relationships between the source files, much like a
 # C++ include graph
 #
-# It can handle most variations of use statement, but not all.  Multi-line
-# statements are a problem.  There are probably others.
-#
-# Usage: Modify EXTERNAL_CRATES as appropriate.  Those relationships will be
-# excluded from the graph.  Then run the script.  "use_graph.svg" will be
-# created in PWD.
+# Usage: Run the script. "use_graph.svg" will be created in PWD.
 
-EXTERNAL_CRATES='(std|blosc|byteorder|divbuf|fixedbitset|futures|isa_l|itertools|metrohash|mio|mockers|modulo|permutohedron|nix|rand|serde|serde_cbor|super|tempdir|tokio|tokio_file|uuid)'
-BORING_FILES='^\<(common)\>$'
-
-EXTERNAL_PAT="^use.${EXTERNAL_CRATES}"
-SRC=`dirname $0`/../src
 TEMPFILE=`mktemp -t use_graph.dot`
-{
-echo "strict digraph use_graph {"
-for f in `find $SRC -name '*.rs'`; do
-	basename=${f#${SRC}/}
-	mod=${basename%.rs}
-	uses=`awk -v external_pat=${EXTERNAL_PAT} '
-		$0 ~ external_pat {next;}
-		/^use .*{/ {
-			sub(/use /, "", $0);
-			gsub(/::/, "/", $0);
-			sub(/sys\//, "sys/fuse/", $0);
-			prefix=$0
-			tail=$0
-			sub(/\/?\{.*/, "", prefix);
-			sub(/^\//, "", prefix);
-			sub(/^.*\{/, "", tail);
-			sub(/\}.*$/, "", tail);
-			gsub(/,/, "", tail);
-			split(tail, modules)
-			OFS="/"
-			if (prefix == "")
-				for (mod in modules)
-					print modules[mod]
-			else
-				for (mod in modules)
-					print prefix, modules[mod]
-		}
-		/^use / {
-			sub(/(::\*)?;/, "", $2);
-			sub(/^::/, "", $2);
-			gsub(/::/, "/", $2);
-			sub(/sys\//, "sys/fuse/", $2);
-			print $2
-		}' $f`
-	for u in $uses; do
-		if [ -f ${SRC}/${u}.rs ]; then
-			usefile=${u}
-		elif [ -f ${SRC}/${u}/mod.rs ]; then
-			usefile=${u}
-		elif [ -f `dirname ${SRC}/${u}.rs`.rs ]; then
-			usefile=`dirname ${u}.rs`
-		else
-			echo "File ${u} not found" >&2
-			continue
-		fi
-		if echo $usefile | egrep -q $BORING_FILES; then
-			# Skip this boring file
-		else
-			echo "\"$mod\" -> \"$usefile\""
-		fi
-	done
-done
-echo "}" ; } > $TEMPFILE
-dot -Tsvg -o use_graph.svg $TEMPFILE
+cargo +nightly-2018-06-01-x86_64-unknown-freebsd modules graph > $TEMPFILE
+sed '
+# Exclude test modules
+/::t::/d
+/::t"/d
+#
+# Exclude boring modules
+/"::arkfs"/d
+/"::common"/d
+/"::common::dva"/d
+/"::common::sgcursor"/d
+/"::common::tree::atomic_usize_serializer"/d
+/"::common::tree::tree_root_serializer"/d
+/"::common::vdev"/d
+#
+# Exclude submodule edges, leaving just the use edges
+/->.*weight=100/d
+' < $TEMPFILE | dot -Tsvg -o use_graph.svg
 rm $TEMPFILE
