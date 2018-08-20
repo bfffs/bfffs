4ba4de1ed0fe9d2aa8a23db490771df4e7e2598c
Alan Somers <asomers@gmail.com>
2018-08-20 15:22:31 -0600

    bfffs: refactor the pool module to encapsulate state into an object

--- src/bin/bfffs.rs
+++ src/bin/bfffs.rs
@@ -42,7 +42,7 @@ pub fn main(args: &clap::ArgMatches) {
 
 }
 
-mod pool{
+mod pool {
 use bfffs::common::LbaT;
 use bfffs::common::cache::Cache;
 use bfffs::common::database::*;
@@ -61,22 +61,21 @@ use tokio::{
 const CHUNKSIZE: LbaT = 16;
 
 fn create(args: &clap::ArgMatches) {
-    let name = args.value_of("name").unwrap();
+    let rt = Runtime::new().unwrap();
+    let name = args.value_of("name").unwrap().to_owned();
+    let mut builder = Builder::new(name, rt);
     let mut values = args.values_of("vdev").unwrap();
-    let mut cluster = None;
-    let mut clusters = vec![];
+    let mut cluster_type = None;
     let mut devs = vec![];
-    let mut rt = Runtime::new().unwrap();
     loop {
         let next = values.next();
         match next {
             None => {
                 if devs.len() > 0 {
-                    match cluster{
+                    match cluster_type {
                         Some("mirror") =>
-                            clusters.push(create_mirror(&mut rt, &devs[..])),
-                        Some("raid") => clusters.push(
-                            create_raid(&mut rt, &devs[..])),
+                            builder.create_mirror(&devs[..]),
+                        Some("raid") => builder.create_raid(&devs[..]),
                         None => assert!(devs.is_empty()),
                         _ => unreachable!()
                     }
@@ -85,82 +84,99 @@ fn create(args: &clap::ArgMatches) {
             },
             Some("mirror") => {
                 if devs.len() > 0 {
-                    let c = create_cluster(&mut rt, cluster.as_ref().unwrap(),
+                    builder.create_cluster(cluster_type.as_ref().unwrap(),
                                            &devs[..]);
-                    clusters.push(c);
                 }
                 devs.clear();
-                cluster = Some("mirror")
+                cluster_type = Some("mirror")
             },
             Some("raid") => {
                 if devs.len() > 0 {
-                    let c = create_cluster(&mut rt, cluster.as_ref().unwrap(),
+                    builder.create_cluster(cluster_type.as_ref().unwrap(),
                                            &devs[..]);
-                    clusters.push(c);
                 }
                 devs.clear();
-                cluster = Some("raid")
+                cluster_type = Some("raid")
             },
             Some(ref dev) => {
-                if cluster == None {
-                    let c = create_single(&mut rt, dev);
-                    clusters.push(c);
+                if cluster_type == None {
+                    builder.create_single(dev);
                 } else {
                     devs.push(dev);
                 }
             }
         }
     }
-    let db = rt.block_on(future::lazy(|| {
-        Pool::create(String::from(name), clusters)
-        .map(|pool| {
-            let cache = Arc::new(Mutex::new(Cache::with_capacity(1000)));
-            let ddml = Arc::new(DDML::new(pool, cache.clone()));
-            let idml = Arc::new(IDML::create(ddml, cache));
-            let task_executor = TaskExecutor::current();
-            Database::create(idml, task_executor)
-        })
-    })).unwrap();
-    rt.block_on(
-        db.new_fs().and_then(|_tree_id| db.sync_transaction())
-    ).unwrap();
+    builder.format()
 }
 
-fn create_cluster(rt: &mut Runtime, vtype: &str, devs: &[&str]) -> ClusterProxy {
-    match vtype {
-        "mirror" => create_mirror(rt, devs),
-        "raid" => create_raid(rt, devs),
-        _ => panic!("Unsupported vdev type {}", vtype)
+struct Builder {
+    clusters: Vec<ClusterProxy>,
+    name: String,
+    rt: Runtime,
+}
+
+impl Builder {
+    pub fn new(name: String, rt: Runtime) -> Self {
+        let clusters = Vec::new();
+        Builder{clusters, name, rt}
     }
-}
 
-fn create_mirror(rt: &mut Runtime, devs: &[&str]) -> ClusterProxy {
-    // TODO: allow creating declustered mirrors
-    let n = devs.len() as i16;
-    let k = devs.len() as i16;
-    let f = devs.len() as i16 - 1;
-    do_create_cluster(rt, n, k, f, &devs[2..])
-}
+    pub fn create_cluster(&mut self, vtype: &str, devs: &[&str]) {
+        match vtype {
+            "mirror" => self.create_mirror(devs),
+            "raid" => self.create_raid(devs),
+            _ => panic!("Unsupported vdev type {}", vtype)
+        }
+    }
 
-fn create_raid(rt: &mut Runtime, devs: &[&str]) -> ClusterProxy {
-    let n = devs.len() as i16 - 2;
-    let k = i16::from_str_radix(devs[0], 10)
-        .expect("Disks per stripe must be an integer");
-    let f = i16::from_str_radix(devs[1], 10)
-        .expect("Disks per stripe must be an integer");
-    do_create_cluster(rt, n, k, f, &devs[2..])
-}
+    pub fn create_mirror(&mut self, devs: &[&str]) {
+        // TODO: allow creating declustered mirrors
+        let n = devs.len() as i16;
+        let k = devs.len() as i16;
+        let f = devs.len() as i16 - 1;
+        self.do_create_cluster(n, k, f, &devs[2..])
+    }
 
-fn create_single(rt: &mut Runtime, dev: &str) -> ClusterProxy {
-    do_create_cluster(rt, 1, 1, 0, &[&dev])
-}
+    pub fn create_raid(&mut self, devs: &[&str]) {
+        let n = devs.len() as i16 - 2;
+        let k = i16::from_str_radix(devs[0], 10)
+            .expect("Disks per stripe must be an integer");
+        let f = i16::from_str_radix(devs[1], 10)
+            .expect("Disks per stripe must be an integer");
+        self.do_create_cluster(n, k, f, &devs[2..])
+    }
 
-fn do_create_cluster(rt: &mut Runtime, n: i16, k: i16, f: i16, devs: &[&str])
-    -> ClusterProxy
-{
-    rt.block_on(future::lazy(move || {
-        Pool::create_cluster(CHUNKSIZE, n, k, None, f, devs)
-    })).unwrap()
+    pub fn create_single(&mut self, dev: &str) {
+        self.do_create_cluster(1, 1, 0, &[&dev])
+    }
+
+    fn do_create_cluster(&mut self, n: i16, k: i16, f: i16, devs: &[&str])
+    {
+        let c = self.rt.block_on(future::lazy(move || {
+            Pool::create_cluster(CHUNKSIZE, n, k, None, f, devs)
+        })).unwrap();
+        self.clusters.push(c);
+    }
+
+    /// Actually format the disks
+    pub fn format(&mut self) {
+        let name = self.name.clone();
+        let clusters = self.clusters.drain(..).collect();
+        let db = self.rt.block_on(future::lazy(|| {
+            Pool::create(name, clusters)
+            .map(|pool| {
+                let cache = Arc::new(Mutex::new(Cache::with_capacity(1000)));
+                let ddml = Arc::new(DDML::new(pool, cache.clone()));
+                let idml = Arc::new(IDML::create(ddml, cache));
+                let task_executor = TaskExecutor::current();
+                Database::create(idml, task_executor)
+            })
+        })).unwrap();
+        self.rt.block_on(
+            db.new_fs().and_then(|_tree_id| db.sync_transaction())
+        ).unwrap();
+    }
 }
 
 pub fn main(args: &clap::ArgMatches) {
