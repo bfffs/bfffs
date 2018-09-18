aafd46079d57236091b45df6f3f3a775bee7921c
Alan Somers <asomers@gmail.com>
2018-09-17 18:51:51 -0600

    tree::ranges_overlap: better test coverage

--- src/common/tree/tests/mod.rs
+++ src/common/tree/tests/mod.rs
@@ -7,10 +7,20 @@ use common::tree::*;
 
 #[test]
 fn ranges_overlap_test() {
+    // x is empty
+    assert!(!ranges_overlap(&(5..5), &(0..10)));
+    // y is empty
+    assert!(!ranges_overlap(&(0..10), &(5..5)));
+    // x precedes y
     assert!(!ranges_overlap(&(0..10), &(10..20)));
+    // x contains y
     assert!(ranges_overlap(&(0..10), &(9..10)));
+    // y contains x
     assert!(ranges_overlap(&(9..10), &(0..10)));
+    // y precedes x
     assert!(!ranges_overlap(&(10..20), &(0..10)));
+    // end of x overlaps start of y
     assert!(ranges_overlap(&(0..10), &(9..11)));
+    // end of y overlaps start of x
     assert!(ranges_overlap(&(9..11), &(0..10)));
 }
