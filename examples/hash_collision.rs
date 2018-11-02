e96debb587bd5c8274d74e434cfd515b1b495cfc
Alan Somers <asomers@gmail.com>
2018-11-02 13:15:16 -0600

    examples/hash_collision: reduce and cap RAM usage
    Reduce RAM usage by not storing the full ExtAttr name and namespace.
    Instead, store a seed that can be used to recreate them.  Cap RAM usage with
    setrlimit, controllable from the command line.

--- examples/hash_collision.rs
+++ examples/hash_collision.rs
@@ -4,7 +4,9 @@
 
 extern crate bfffs;
 extern crate chashmap;
+extern crate clap;
 extern crate lazy_static;
+extern crate libc;
 extern crate num_cpus;
 extern crate rand;
 extern crate rand_xorshift;
@@ -16,6 +18,7 @@ use rand_xorshift::XorShiftRng;
 use rand::{
     FromEntropy,
     Rng,
+    SeedableRng,
     distributions::Alphanumeric
 };
 use std::{
@@ -31,9 +34,24 @@ const NAMESPACES: [ExtAttrNamespace; 2] =
 
 lazy_static! {
     // CHashMap resizes more slowly than the standard hashmap.  So give it a
-    // large size to start.
-    static ref HM: CHashMap<u64, (ExtAttrNamespace, OsString)> = 
-        CHashMap::with_capacity(64_000_000);
+    // large size to start.  64M entries takes about 10GB.
+    //
+    // Don't store the actual namespace and name, because that takes too much
+    // RAM.  Instead, store a seed that can be used to recreate the name and
+    // namespace.  It cuts the throughput, but also cuts the RAM usage.
+    static ref HM: CHashMap<u64, [u8; 16]> =
+        CHashMap::with_capacity(4_000_000);
+}
+
+fn mkname(seed: &[u8; 16]) -> (ExtAttrNamespace, OsString) {
+    let mut this_rng = XorShiftRng::from_seed(*seed);
+    let v: Vec<u8> = this_rng.sample_iter(&Alphanumeric)
+        .map(|c| c as u8)
+        .take(10)
+        .collect();
+    let name = OsStr::from_bytes(&v[..]);
+    let ns = this_rng.choose(&NAMESPACES).unwrap();
+    (*ns, name.to_owned())
 }
 
 fn report(collisions: u64, tries: u64) {
@@ -41,13 +59,13 @@ fn report(collisions: u64, tries: u64) {
 }
 
 struct Worker {
-    hm: &'static CHashMap<u64, (ExtAttrNamespace, OsString)>,
+    hm: &'static CHashMap<u64, [u8; 16]>,
     rng: XorShiftRng,
     tx: mpsc::Sender<(u64, u64)>
 }
 
 impl Worker {
-    fn new(hm: &'static CHashMap<u64, (ExtAttrNamespace, OsString)>,
+    fn new(hm: &'static CHashMap<u64, [u8; 16]>,
            tx: mpsc::Sender<(u64, u64)>) -> Self
     {
         let rng = XorShiftRng::from_entropy();
@@ -66,20 +84,18 @@ impl Worker {
         let mut tries = 0u64;
         let mut collisions = 0u64;
         for _ in 0..10_000 {
-            let v: Vec<u8> = self.rng.sample_iter(&Alphanumeric)
-                .map(|c| c as u8)
-                .take(10)
-                .collect();
-            let name = OsStr::from_bytes(&v[..]);
-            let ns = self.rng.choose(&NAMESPACES).unwrap();
-            let objkey = ObjKey::extattr(*ns, &name);
+            let seed: [u8; 16] = self.rng.gen();
+            let (ns, name) = mkname(&seed);
+            let objkey = ObjKey::extattr(ns, &name);
             tries += 1;
             if let ObjKey::ExtAttr(offset) = objkey {
-                let shortoffset = offset;// & ((1<<36) - 1);
-                let v = (*ns, name.to_owned());
-                if let Some((oldns, oldname)) = self.hm.insert(shortoffset, v) {
+                // For testing hash_collision.rs itself, shorten the offset
+                //let offset = offset & ((1<<52) - 1);
+                let v = seed;
+                if let Some(old_seed) = self.hm.insert(offset, v) {
+                    let (old_ns, old_name) = mkname(&old_seed);
                     println!("Hash collision: {:?} and {:?} have offset {:?}",
-                             (ns, name), (oldns, oldname), shortoffset);
+                             (ns, name), (old_ns, old_name), offset);
                     collisions += 1;
                     continue;
                 }
@@ -92,6 +108,23 @@ impl Worker {
 }
 
 fn main() {
+    let app = clap::App::new("hash_collision")
+    .about("Generate hash collisions for dirent and extattr storage in BFFFS")
+    .arg(clap::Arg::with_name("mem")
+         .help("Memory limit in GB")
+         .required(true)
+    );
+    let matches = app.get_matches();
+    let limit: i64 = matches.value_of("mem").unwrap().parse().unwrap();
+    unsafe {
+        // Limit RAM usage
+        let rlimit = libc::rlimit{
+            rlim_cur: limit*(1<<30),
+            rlim_max: limit*(1<<30)
+        };
+        libc::setrlimit(libc::RLIMIT_AS, &rlimit);
+    }
+
     let mut now = time::Instant::now();
     let mut tries = 0u64;
     let mut collisions = 0u64;
