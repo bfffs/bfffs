2a3b8e18d142818ffc674617869d87a87c853b32
Alan Somers <asomers@gmail.com>
2018-11-28 22:27:41 -0700

    DevManager: factor out common code

--- src/common/device_manager.rs
+++ src/common/device_manager.rs
@@ -37,37 +37,18 @@ impl DevManager {
     pub fn import<E: Clone + Executor + 'static>(&self, uuid: Uuid, handle: E)
         -> impl Future<Item = database::Database, Error = Error>
     {
-        let (_pool, raids, mut leaves) = {
-            let mut inner = self.inner.lock().unwrap();
-            let pool = inner.pools.remove(&uuid).unwrap();
-            let raids = pool.children.iter()
-                .map(|child_uuid| inner.raids.remove(child_uuid).unwrap())
-                .collect::<Vec<_>>();
-            let leaves = raids.iter().map(|raid| {
-                let leaves = raid.children.iter().map(|uuid| {
-                    inner.leaves.remove(&uuid).unwrap()
-                }).collect::<Vec<_>>();
-                (raid.uuid, leaves)
-            }).collect::<BTreeMap<_, _>>();
-            // Drop the self.inner mutex
-            (pool, raids, leaves)
-        };
+        let (_pool, raids, mut leaves) = self.open_labels(uuid);
         let proxies = raids.into_iter().map(move |raid| {
             let leaf_paths: Vec<PathBuf> = leaves.remove(&raid.uuid).unwrap();
             let (tx, rx) = oneshot::channel();
             // The top-level Executor spawn puts each Cluster onto a different
             // thread, when using tokio-io-pool
             DefaultExecutor::current().spawn(Box::new(future::lazy(move || {
-                let fut = DevManager::open_vdev_blocks(leaf_paths)
-                .and_then(move |vdev_blocks| {
-                    let (vdev_raid, reader) =
-                        vdev_raid::VdevRaid::open(Some(raid.uuid), vdev_blocks);
-                    cluster::Cluster::open(vdev_raid)
-                    .map(move |cluster| {
-                        let proxy = pool::ClusterProxy::new(cluster);
-                        tx.send((proxy, reader))
-                            .ok().expect("channel dropped too soon");
-                    })
+                let fut = DevManager::open_cluster(leaf_paths, raid.uuid)
+                .map(move |(cluster, reader)| {
+                    let proxy = pool::ClusterProxy::new(cluster);
+                    tx.send((proxy, reader))
+                        .ok().expect("channel dropped too soon");
                 }).map_err(Error::unhandled);
                 executor::current_thread::TaskExecutor::current().spawn_local(
                     Box::new(fut)
@@ -95,29 +76,11 @@ impl DevManager {
     pub fn import_clusters(&self, uuid: Uuid)
         -> impl Future<Item = Vec<cluster::Cluster>, Error = Error>
     {
-        let (_pool, raids, mut leaves) = {
-            let mut inner = self.inner.lock().unwrap();
-            let pool = inner.pools.remove(&uuid).unwrap();
-            let raids = pool.children.iter()
-                .map(|child_uuid| inner.raids.remove(child_uuid).unwrap())
-                .collect::<Vec<_>>();
-            let leaves = raids.iter().map(|raid| {
-                let leaves = raid.children.iter().map(|uuid| {
-                    inner.leaves.remove(&uuid).unwrap()
-                }).collect::<Vec<_>>();
-                (raid.uuid, leaves)
-            }).collect::<BTreeMap<_, _>>();
-            // Drop the self.inner mutex
-            (pool, raids, leaves)
-        };
+        let (_pool, raids, mut leaves) = self.open_labels(uuid);
         let cfuts = raids.into_iter().map(move |raid| {
             let leaf_paths = leaves.remove(&raid.uuid).unwrap();
-            DevManager::open_vdev_blocks(leaf_paths)
-            .and_then(move |vdev_blocks| {
-                let (vdev_raid, _label_reader) =
-                    vdev_raid::VdevRaid::open(Some(raid.uuid), vdev_blocks);
-                cluster::Cluster::open(vdev_raid)
-            })
+            DevManager::open_cluster(leaf_paths, raid.uuid)
+            .map(|(cluster, _reader)| cluster)
         });
         future::join_all(cfuts)
     }
@@ -129,6 +92,38 @@ impl DevManager {
             .map(|(_uuid, label)| {
                 (label.name.clone(), label.uuid)
             }).collect::<Vec<_>>()
+    }
+
+    #[cfg(not(test))]
+    fn open_cluster(leaf_paths: Vec<PathBuf>, uuid: Uuid)
+        -> impl Future<Item=(cluster::Cluster, label::LabelReader), Error=Error>
+    {
+        DevManager::open_vdev_blocks(leaf_paths)
+        .and_then(move |vdev_blocks| {
+            let (vdev_raid, reader) =
+                vdev_raid::VdevRaid::open(Some(uuid), vdev_blocks);
+            cluster::Cluster::open(vdev_raid)
+            .map(move |cluster| (cluster, reader))
+        })
+    }
+
+    #[cfg(not(test))]
+    fn open_labels(&self, uuid: Uuid)
+        -> (pool::Label, Vec<vdev_raid::Label>, BTreeMap<Uuid, Vec<PathBuf>>)
+    {
+        let mut inner = self.inner.lock().unwrap();
+        let pool = inner.pools.remove(&uuid).unwrap();
+        let raids = pool.children.iter()
+            .map(|child_uuid| inner.raids.remove(child_uuid).unwrap())
+            .collect::<Vec<_>>();
+        let leaves = raids.iter().map(|raid| {
+            let leaves = raid.children.iter().map(|uuid| {
+                inner.leaves.remove(&uuid).unwrap()
+            }).collect::<Vec<_>>();
+            (raid.uuid, leaves)
+        }).collect::<BTreeMap<_, _>>();
+        // Drop the self.inner mutex
+        (pool, raids, leaves)
     }
 
     #[cfg(not(test))]
