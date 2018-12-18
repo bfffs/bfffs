be8d5c336bd9d6d137f12ec55001c4d8065d5953
Alan Somers <asomers@gmail.com>
2018-12-17 20:23:41 -0700

    Run benchmarks in debug mode in test.sh
    See Also https://github.com/rust-lang/cargo/issues/6454

--- test.sh
+++ test.sh
@@ -2,13 +2,8 @@
 set -e
 
 # Run all of the tests in order of most likely to fail to least likely
-
-cargo +nightly test --all-features --all
-
-# Check that benchmarks build, but don't run them.  Building them in release
-# mode is too slow.  TODO: build and run them in debug mode once this PR lands:
-# https://github.com/rust-lang/cargo/pull/6446#issuecomment-447582775
-cargo +nightly build --all-features --benches --all
+# Use --all-targets to include benchmarks (in debug mode)
+cargo +nightly test --all-features --all --all-targets
 
 # It should also work on stable >= 1.30.0
 cargo +stable test --all
