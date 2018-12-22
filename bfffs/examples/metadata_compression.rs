a1daa37b98267761add0912f6cf985864287f262
Alan Somers <asomers@gmail.com>
2018-12-22 10:11:21 -0700

    examples/metadata_compression: optionally apply a shuffle

--- bfffs/examples/metadata_compression.rs
+++ bfffs/examples/metadata_compression.rs
@@ -17,18 +17,23 @@ const COMPRESSORS: [blosc::Compressor; 6] = [
     blosc::Compressor::Zstd
 ];
 
-const DATASETS: [&str; 3] = [
-    &"alloct",
-    &"ridt",
-    &"fs"
+const DATASETS: [(&str, usize); 3] = [
+    (&"alloct", 18),
+    (&"ridt", 46),
+    (&"fs", 32)
 ];
 
 fn main() {
-    println!("                   min   mean    max stddev");
-    for ds in DATASETS.iter() {
-        let mut histos = HashMap::new();
+    println!("                shuffle    min   mean    max stddev");
+    for (ds, typesize) in DATASETS.iter() {
+        let mut histos = Vec::new();
+        for _shuf in &[true, false] {
+            histos.push(HashMap::new());
+        }
         for z in COMPRESSORS.iter() {
-            histos.insert(z, Histogram::new());
+            for shuf in &[true, false] {
+                histos[*shuf as usize].insert(z, Histogram::new());
+            }
         }
         let pat = format!("/tmp/fanout/{}.*.bin", ds);
         for path in glob::glob(&pat).unwrap() {
@@ -36,27 +41,38 @@ fn main() {
             let mut buf = Vec::new();
             let lsize = f.read_to_end(&mut buf).unwrap();
             for z in COMPRESSORS.iter() {
-                let zbuf = blosc::Context::new()
-                    .compressor(*z)
-                    .unwrap()
-                    .compress(&buf[0..lsize]);
-                let csize = zbuf.size();
-                let ratio = csize as f64 / lsize as f64;
-                // The histogram crate only works with u64 data
-                histos.get_mut(&z).unwrap()
-                    .increment((1000.0 * ratio) as u64)
-                    .unwrap();
+                for shuf in &[true, false] {
+                    let shufmode = if *shuf {
+                        blosc::ShuffleMode::Byte
+                    } else {
+                        blosc::ShuffleMode::None
+                    };
+                    let zbuf = blosc::Context::new()
+                        .compressor(*z)
+                        .unwrap()
+                        .shuffle(shufmode)
+                        .typesize(Some(*typesize))
+                        .compress(&buf[0..lsize]);
+                    let csize = zbuf.size();
+                    let ratio = csize as f64 / lsize as f64;
+                    // The histogram crate only works with u64 data
+                    histos[*shuf as usize].get_mut(&z).unwrap()
+                        .increment((1000.0 * ratio) as u64)
+                        .unwrap();
+                }
             }
         }
         for z in COMPRESSORS.iter() {
-            let hs = &histos[z];
-            let zname = format!("{:?}", z);
-            let min = hs.minimum().unwrap() as f64 / 10.0;
-            let mean = hs.mean().unwrap() as f64 / 10.0;
-            let max = hs.maximum().unwrap() as f64 / 10.0;
-            let stddev = hs.stddev().unwrap() as f64 / 10.0;
-            println!("{:8}{:8}{:5.1}% {:5.1}% {:5.1}% {:5.1}%", ds, zname,
-                     min, mean, max, stddev);
+            for shuf in &[true, false] {
+                let hs = &histos[*shuf as usize][z];
+                let zname = format!("{:?}", z);
+                let min = hs.minimum().unwrap() as f64 / 10.0;
+                let mean = hs.mean().unwrap() as f64 / 10.0;
+                let max = hs.maximum().unwrap() as f64 / 10.0;
+                let stddev = hs.stddev().unwrap() as f64 / 10.0;
+                println!("{:8}{:8}{:8}{:5.1}% {:5.1}% {:5.1}% {:5.1}%", ds,
+                         zname, shuf, min, mean, max, stddev);
+            }
         }
     }
 }
