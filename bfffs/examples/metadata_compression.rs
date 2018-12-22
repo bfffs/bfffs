ba3eaaea4fe9ea38b336706ebb150e4b3167d7f8
Alan Somers <asomers@gmail.com>
2018-12-22 11:13:34 -0700

    examples/metadata_compression: try the bit filter, too
    As expected, the bit filter runs slower.  It produces marginally better
    compression for the RIDT and FS trees.  Unexpectedly, it produces
    substantially *worse* compression for the AllocT.
    
    Experimenting with blosc's clevel didn't produce significant improvements
    over the default.

--- bfffs/examples/metadata_compression.rs
+++ bfffs/examples/metadata_compression.rs
@@ -26,6 +26,12 @@ const DATASETS: [(&str, usize); 3] = [
     (&"fs", 32)
 ];
 
+const SHUFFLES: [(&str, blosc::ShuffleMode); 3] = [
+    (&"none", blosc::ShuffleMode::None),
+    (&"byte", blosc::ShuffleMode::Byte),
+    (&"bit", blosc::ShuffleMode::Bit),
+];
+
 fn main() {
     println!("tree    algo    shuffle |      compression ratio      | compression times");
     println!("                        |    min   mean    max stddev |     mean   stddev");
@@ -35,14 +41,14 @@ fn main() {
         let mut ratios = Vec::new();
         // Compression times in nanoseconds
         let mut times = Vec::new();
-        for _shuf in &[true, false] {
+        for _ in SHUFFLES.iter().enumerate() {
             ratios.push(HashMap::new());
             times.push(HashMap::new());
         }
         for z in COMPRESSORS.iter() {
-            for shuf in &[true, false] {
-                ratios[*shuf as usize].insert(z, Histogram::new());
-                times[*shuf as usize].insert(z, Histogram::new());
+            for (i, _) in SHUFFLES.iter().enumerate() {
+                ratios[i].insert(z, Histogram::new());
+                times[i].insert(z, Histogram::new());
             }
         }
         let pat = format!("/tmp/fanout/{}.*.bin", ds);
@@ -51,50 +57,45 @@ fn main() {
             let mut buf = Vec::new();
             let lsize = f.read_to_end(&mut buf).unwrap();
             for z in COMPRESSORS.iter() {
-                for shuf in &[true, false] {
-                    let shufmode = if *shuf {
-                        blosc::ShuffleMode::Byte
-                    } else {
-                        blosc::ShuffleMode::None
-                    };
+                for (i, (_, shufmode)) in SHUFFLES.iter().enumerate() {
                     let start = time::Instant::now();
                     let zbuf = blosc::Context::new()
                         .compressor(*z)
                         .unwrap()
-                        .shuffle(shufmode)
+                        .shuffle(*shufmode)
                         .typesize(Some(*typesize))
                         .compress(&buf[0..lsize]);
 
                     let time = start.elapsed();
                     debug_assert_eq!(time.as_secs(), 0);
-                    times[*shuf as usize].get_mut(&z).unwrap()
+                    times[i].get_mut(&z).unwrap()
                         .increment(time.subsec_nanos().into())
                         .unwrap();
 
                     let csize = zbuf.size();
                     let ratio = csize as f64 / lsize as f64;
-                    ratios[*shuf as usize].get_mut(&z).unwrap()
+                    ratios[i].get_mut(&z).unwrap()
                         .increment((1000.0 * ratio) as u64)
                         .unwrap();
                 }
             }
         }
         for z in COMPRESSORS.iter() {
-            for shuf in &[true, false] {
+            for (i, (shufname, _)) in SHUFFLES.iter().enumerate() {
                 let zname = format!("{:?}", z);
 
-                let ratio = &ratios[*shuf as usize][z];
+                let ratio = &ratios[i][z];
                 let zmin = ratio.minimum().unwrap() as f64 / 10.0;
                 let zmean = ratio.mean().unwrap() as f64 / 10.0;
                 let zmax = ratio.maximum().unwrap() as f64 / 10.0;
                 let zstddev = ratio.stddev().unwrap() as f64 / 10.0;
 
-                let time = &times[*shuf as usize][z];
+                let time = &times[i][z];
                 let tmean = time.mean().unwrap();
                 let tstddev = time.stddev().unwrap();
 
                 print!("{:8}{:8}{:8}| {:5.1}% {:5.1}% {:5.1}% {:5.1}%", ds,
-                         zname, shuf, zmin, zmean, zmax, zstddev);
+                         zname, shufname, zmin, zmean, zmax, zstddev);
                 println!(" | {:6.1}ns {:6.1}ns",
                          tmean, tstddev);
             }
