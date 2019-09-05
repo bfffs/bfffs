03532025f6bf21bfc5a30461536592b45d9910fe
Alan Somers <asomers@gmail.com>
2019-09-05 12:35:49 -0600

    bfffs-fuse: allow passing arbitrary mount options to the kernel

--- bfffs-fuse/src/bin/bfffsd/main.rs
+++ bfffs-fuse/src/bin/bfffsd/main.rs
@@ -10,8 +10,7 @@ use bfffs::{
 use clap::crate_version;
 use futures::{Future, Stream, future};
 use std::{
-    ffi::OsStr,
-    os::unix::ffi::OsStrExt,
+    ffi::OsString,
     sync::Arc,
     thread
 };
@@ -25,7 +24,13 @@ fn main() {
     env_logger::init();
     let app = clap::App::new("bfffsd")
         .version(crate_version!())
-        .arg(clap::Arg::with_name("name")
+        .arg(clap::Arg::with_name("option")
+             .help("Mount options")
+             .short("o")
+             .takes_value(true)
+             .multiple(true)
+             .require_delimiter(true)
+        ).arg(clap::Arg::with_name("name")
              .help("Pool name")
              .required(true)
          ).arg(clap::Arg::with_name("mountpoint")
@@ -35,6 +40,16 @@ fn main() {
              .multiple(true)
          );
     let matches = app.get_matches();
+
+    // Unconditionally disable the kernel's buffer cache since BFFFS has its own
+    let mut opts = vec![OsString::from("-o"), OsString::from("direct_io")];
+    if let Some(it) = matches.values_of("option") {
+        for o in it {
+            opts.push(OsString::from("-o"));
+            opts.push(OsString::from(o));
+        }
+    };
+
     let poolname = matches.value_of("name").unwrap().to_string();
     let mountpoint = matches.value_of("mountpoint").unwrap().to_string();
     let devices = matches.values_of("devices").unwrap()
@@ -61,11 +76,10 @@ fn main() {
     let db2 = db.clone();
     let thr_handle = thread::spawn(move || {
         let fs = FuseFs::new(db, handle2, tree_id);
-        let opts = [
-            OsStr::from_bytes(b"-o"),
-            OsStr::from_bytes(b"direct_io"),
-        ];
-        fs::mount(fs, &mountpoint, &opts[..]).unwrap();
+        // We need a separate vec of references :(
+        // https://github.com/zargony/rust-fuse/issues/117
+        let opt_refs = opts.iter().map(|o| o.as_ref()).collect::<Vec<_>>();
+        fs::mount(fs, &mountpoint, &opt_refs[..]).unwrap();
     });
 
     // Run the cleaner on receipt of SIGUSR1.  While not ideal long-term, this
