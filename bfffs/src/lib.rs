fedb0eb550e74b805f126ccbc2e94cbf76e442f9
Alan Somers <asomers@gmail.com>
2020-11-08 14:29:06 -0700

    Delete some now-unused macros

--- bfffs/src/lib.rs
+++ bfffs/src/lib.rs
@@ -22,26 +22,3 @@
 extern crate test;
 
 pub mod common;
-
-#[macro_export]
-macro_rules! boxfut {
-    ( $v:expr ) => {
-        Box::new($v) as Box<dyn Future<Item=_, Error=_> + Send>
-    };
-    ( $v:expr, $e:ty ) => {
-        Box::new($v) as Box<dyn Future<Item=_, Error=$e> + Send>
-    };
-    ( $v:expr, $i:ty, $e:ty ) => {
-        Box::new($v) as Box<dyn Future<Item=$i, Error=$e> + Send>
-    };
-    ( $v:expr, $i:ty, $e:ty, $lt:lifetime ) => {
-        Box::new($v) as Box<dyn Future<Item=$i, Error=$e> + $lt>
-    };
-}
-
-#[macro_export]
-macro_rules! boxstream {
-    ( $v:expr ) => {
-        Box::new($v) as Box<dyn Stream<Item=_, Error=_> + Send>
-    };
-}
