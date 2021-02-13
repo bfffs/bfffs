6441257f6b6d103b2e1f0e5753baec96c2e1cee2
Alan Somers <asomers@gmail.com>
2021-02-13 10:35:12 -0700

    Update Mockall to 0.9.1

--- bfffs-core/src/tree/tree/mod.rs
+++ bfffs-core/src/tree/tree/mod.rs
@@ -35,7 +35,7 @@ use std::{
 };
 use super::*;
 use tokio::runtime;
-use tracing::{Level, instrument};
+use tracing::instrument;
 use tracing_futures::Instrument;
 
 #[cfg(test)] mod tests;
@@ -177,13 +177,10 @@ impl<A, D, K, T, V> Stream for RangeQuery<A, D, K, T, V>
 {
     type Item = Result<(K, V), Error>;
 
+    #[instrument(skip(self, cx))]
     fn poll_next<'a>(mut self: Pin<&mut Self>, cx: &mut Context<'a>)
         -> Poll<Option<Self::Item>>
     {
-        // Note: #[instrument] is currently incompatible with Mockall.  The
-        // following is equivalent.
-        let span = tracing::span!(Level::INFO, "poll_next");
-        let _enter = span.enter();
         self.as_mut()
             .pin_get_data()
             .pop_front()
