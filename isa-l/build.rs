1606b67c352772295e9a8db63fcf567a5c0401ce
Alan Somers <asomers@gmail.com>
2021-11-14 10:53:27 -0700

    Don't unnecessarily rebuild isa-l
    Cargo usually figures out that it doesn't need to rebuild the isa-l
    crate.  However, when RUSTFLAGS="-zinstrument-coverage", it rebuilds it
    every time, for some reason.  Explicitly tell it not to.

--- isa-l/build.rs
+++ isa-l/build.rs
@@ -1,6 +1,9 @@
 // vim: tw=80
 
 fn main() {
+    // Avoid unnecessary re-building.
+    println!("cargo:rerun-if-changed=build.rs");
+
     println!("cargo:rustc-link-search=native=/usr/local/lib");
     println!("cargo:rustc-link-lib=isal");
 }
