1e420089066a461a962f167e6e56131ce0ed6199
Alan Somers <asomers@gmail.com>
2022-05-29 10:49:05 -0600

    Rename cache::cache to cache::lru
    Two reasons:
    * Makes it easier to substitute a different cache implementation
    * Will allow the implementation of core cache logic to be separated from
      the stuff involving futures: pending insertions.

--- bfffs-core/src/cache/mod.rs
+++ bfffs-core/src/cache/mod.rs
@@ -11,8 +11,7 @@ use std::{
     fmt::Debug,
 };
 
-mod cache;
-pub use self::cache::Cache;
+mod lru;
 
 /// Key types used by `Cache`
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
@@ -114,5 +113,72 @@ impl CacheRef for DivBuf {
 impl Borrow<dyn CacheRef> for DivBuf {
     fn borrow(&self) -> &dyn CacheRef {
         self as &dyn CacheRef
+    }
+}
+
+/// Basic read-only block cache.
+///
+/// Caches on-disk blocks by either their address (cluster and LBA pair), or
+/// their Record ID.  The cache is read-only because any attempt to change a
+/// block would also require changing either its address or record ID.
+#[derive(Debug)]
+pub struct Cache(self::lru::LruCache);
+
+impl Cache {
+    /// Get the maximum memory consumption of the cache, in bytes.
+    pub fn capacity(&self) -> usize {
+        self.0.capacity()
+    }
+
+    /// Drop all data from the cache, for testing or benchmarking purposes
+    // NB: this should be called "drop", but that conflicts with
+    // "std::Drop::drop"
+    pub fn drop_cache(&mut self) {
+        self.0.drop_cache()
+    }
+
+    /// Get a read-only reference to a cached block.
+    ///
+    /// The block will be marked as the most recently used.
+    pub fn get<T: CacheRef>(&mut self, key: &Key) -> Option<Box<T>> {
+        self.0.get(key)
+    }
+
+    /// Get a read-only generic reference to a cached block.
+    ///
+    /// The returned reference will not be downcastted to a concrete type, and
+    /// the cache's internal state will not be updated.  That is, this method
+    /// does not count as an access for the cache replacement algorithm.
+    pub fn get_ref(&self, key: &Key) -> Option<Box<dyn CacheRef>> {
+        self.0.get_ref(key)
+    }
+
+    /// Add a new block to the cache.
+    ///
+    /// The block will be marked as the most recently used.
+    #[tracing::instrument(skip(self, buf))]
+    pub fn insert(&mut self, key: Key, buf: Box<dyn Cacheable>) {
+        self.0.insert(key, buf)
+    }
+
+    /// Remove a block from the cache.
+    ///
+    /// Unlike `get`, the block will be returned in an owned form, if it was
+    /// present at all.
+    pub fn remove(&mut self, key: &Key) -> Option<Box<dyn Cacheable>> {
+        self.0.remove(key)
+    }
+
+    /// Get the current memory consumption of the cache, in bytes.
+    ///
+    /// Only the cached blocks themselves are included, not the overhead of
+    /// managing them.
+    pub fn size(&self) -> usize {
+        self.0.size()
+    }
+
+    /// Create a new cache with the given capacity, in bytes.
+    pub fn with_capacity(capacity: usize) -> Self {
+        Self(self::lru::LruCache::with_capacity(capacity))
     }
 }
