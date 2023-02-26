f416a5e73f033590a3cc70ecb77e2bf4c9ecedfe
Alan Somers <asomers@gmail.com>
2023-02-26 08:41:22 -0700

    Don't rerun bfffs's build.rs script unnecessarily

--- bfffs/build.rs
+++ bfffs/build.rs
@@ -1,3 +1,7 @@
 fn main() {
+    // Avoid unnecessary re-building.
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=src/pool_create_parser.lalrpop");
+
     lalrpop::process_root().unwrap();
 }
