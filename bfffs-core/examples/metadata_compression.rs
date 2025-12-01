d3d8ddec411f899e7c5799c4c46c2fdf3b417820
Alan Somers <asomers@gmail.com>
2025-11-30 17:09:30 -0700

    Add byteshuffle to the metadata_compression example
    Add compression modes that use byteshuffle+lz4_flex and
    byteshuffle+zstd, which accomplish the same thing as Blosc, but with a
    different implementation.  This is the first step in eliminating Blosc.
    
    The new modes generally get about the same speed as blosc, but with
    better compression ratios.

--- bfffs-core/examples/metadata_compression.rs
+++ bfffs-core/examples/metadata_compression.rs
@@ -4,7 +4,6 @@
 //! This program compares different BLOSC algorithms and settings on binary
 //! metadata nodes as produced by `examples/fanout --save`
 
-use histogram::Histogram;
 use std::{
     collections::HashMap,
     io::Read,
@@ -12,13 +11,45 @@ use std::{
     time
 };
 
-const COMPRESSORS: [blosc::Compressor; 6] = [
-    blosc::Compressor::BloscLZ,
-    blosc::Compressor::LZ4,
-    blosc::Compressor::LZ4HC,
-    blosc::Compressor::Snappy,
-    blosc::Compressor::Zlib,
-    blosc::Compressor::Zstd
+use byteshuffle::shuffle;
+use histogram::Histogram;
+use lz4_flex::block as lz4;
+use zstd::bulk as zstd;
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+enum Compressor {
+    Blosc(blosc::Compressor),
+    Lz4Flex,
+    Zstd
+}
+
+#[derive(Clone, Debug)]
+struct Score {
+    compressor: Compressor,
+    shuffle: &'static str,
+    /// Compression ratio in parts per thousand
+    ratio: Histogram,
+    /// Compression speed in MiBps
+    speed: Histogram
+}
+
+impl Score {
+    fn new(compressor: Compressor, shuffle: &'static str) -> Self {
+        let ratio = Histogram::new();
+        let speed = Histogram::new();
+        Self { compressor, shuffle, ratio, speed}
+    }
+}
+
+const COMPRESSORS: &[Compressor] = &[
+    Compressor::Blosc(blosc::Compressor::BloscLZ),
+    Compressor::Blosc(blosc::Compressor::LZ4),
+    Compressor::Lz4Flex,
+    Compressor::Blosc(blosc::Compressor::LZ4HC),
+    Compressor::Blosc(blosc::Compressor::Snappy),
+    Compressor::Blosc(blosc::Compressor::Zlib),
+    Compressor::Blosc(blosc::Compressor::Zstd),
+    Compressor::Zstd,
 ];
 
 // For each pair of trees, the first entry is for leaf nodes and the second for
@@ -39,25 +70,12 @@ const SHUFFLES: [(&str, blosc::ShuffleMode); 3] = [
 ];
 
 fn main() {
-    println!("tree      algo    shuffle |      compression ratio      | compression speeds");
-    println!("                          |    min   mean    max stddev |       mean    stddev");
-    println!("--------------------------+-----------------------------+---------------------");
+    println!("tree      algo           shuffle |      compression ratio      | compression speeds");
+    println!("                                 |    min   mean    max stddev |       mean     stddev");
+    println!("---------------------------------+-----------------------------+---------------------");
     for (ds, typesize) in DATASETS.iter() {
         let mut found = false;
-        // Compression ratios in parts per thousand
-        let mut ratios = Vec::new();
-        // Compression speeds in MiBps
-        let mut speeds = Vec::new();
-        for _ in SHUFFLES.iter().enumerate() {
-            ratios.push(HashMap::new());
-            speeds.push(HashMap::new());
-        }
-        for z in COMPRESSORS.iter() {
-            for (i, _) in SHUFFLES.iter().enumerate() {
-                ratios[i].insert(z, Histogram::new());
-                speeds[i].insert(z, Histogram::new());
-            }
-        }
+        let mut scores = HashMap::<(Compressor, &'static str), Score>::new();
         let pat = format!("/tmp/fanout/{ds}.*.bin");
         for path in glob::glob(&pat).unwrap() {
             found = true;
@@ -66,14 +84,58 @@ fn main() {
             let mut buf = Vec::new();
             let lsize = f.read_to_end(&mut buf).unwrap();
             for z in COMPRESSORS.iter() {
-                for (i, (_, shufmode)) in SHUFFLES.iter().enumerate() {
+                for (shufname, shufmode) in SHUFFLES.iter() {
                     let start = time::Instant::now();
-                    let zbuf = blosc::Context::new()
-                        .compressor(*z)
-                        .unwrap()
-                        .shuffle(*shufmode)
-                        .typesize(Some(*typesize))
-                        .compress(&buf[0..lsize]);
+                    let (zbuf, csize_adjust) = match z {
+                        Compressor::Blosc(z) => {
+                            let zbuf = blosc::Context::new()
+                                .compressor(*z)
+                                .unwrap()
+                                .shuffle(*shufmode)
+                                .typesize(Some(*typesize))
+                                .compress(&buf[0..lsize])
+                                .into();
+                            (zbuf, 0)
+                        },
+                        Compressor::Lz4Flex => {
+                            let zbuf = match shufmode {
+                                blosc::ShuffleMode::Byte => {
+                                    let shuffled = shuffle(*typesize,
+                                        &buf[0..lsize]);
+                                    lz4::compress(&shuffled[..])
+                                },
+                                blosc::ShuffleMode::None => 
+                                    lz4::compress(&buf[0..lsize]),
+                                _ => {
+                                    // No bitshuffle outside of Blosc.
+                                    continue;
+                                }
+                            };
+                            // Add an extra 5 bytes due to the need for us to
+                            // manually tag the compressed buffer.
+                            (zbuf, 5)
+                        }
+                        Compressor::Zstd => {
+                            let zbuf = match shufmode {
+                                blosc::ShuffleMode::Byte => {
+                                    let shuffled = shuffle(*typesize,
+                                        &buf[0..lsize]);
+                                    zstd::compress(&shuffled[..], 0).unwrap()
+                                },
+                                blosc::ShuffleMode::None => 
+                                    zstd::compress(&buf[0..lsize], 0).unwrap(),
+                                _ => {
+                                    // No bitshuffle outside of Blosc.
+                                    continue;
+                                }
+                            };
+                            // Add an extra 5 bytes due to the need for us to
+                            // manually tag the compressed buffer.
+                            (zbuf, 5)
+                        }
+                    };
+                    let score: &mut Score = scores.entry((*z, shufname))
+                        .or_insert_with(|| Score::new(*z, shufname));
 
                     let time = start.elapsed();
                     debug_assert_eq!(time.as_secs(), 0);
@@ -81,13 +143,13 @@ fn main() {
                         * 1_000_000_000
                         / u64::from(time.subsec_nanos())
                         / 1024 / 1024;
-                    speeds[i].get_mut(&z).unwrap()
+                    score.speed
                         .increment(speed)
                         .unwrap();
 
-                    let csize = zbuf.size();
+                    let csize = zbuf.len() + csize_adjust;
                     let ratio = csize as f64 / lsize as f64;
-                    ratios[i].get_mut(&z).unwrap()
+                    score.ratio
                         .increment((1000.0 * ratio) as u64)
                         .unwrap();
                 }
@@ -97,23 +159,22 @@ fn main() {
             eprintln!("No data!  Run examples/fanout first");
             process::exit(1);
         }
-        for z in COMPRESSORS.iter() {
-            for (i, (shufname, _)) in SHUFFLES.iter().enumerate() {
-                let zname = format!("{z:?}");
-
-                let ratio = &ratios[i][z];
-                let zmin = ratio.minimum().unwrap() as f64 / 10.0;
-                let zmean = ratio.mean().unwrap() as f64 / 10.0;
-                let zmax = ratio.maximum().unwrap() as f64 / 10.0;
-                let zstddev = ratio.stddev().unwrap() as f64 / 10.0;
-
-                let speed = &speeds[i][z];
-                let tmean = speed.mean().unwrap();
-                let tstddev = speed.stddev().unwrap();
-
-                print!("{ds:10}{zname:8}{shufname:8}| {zmin:5.1}% {zmean:5.1}% {zmax:5.1}% {zstddev:5.1}%");
-                println!(" | {tmean:5.1}MiBps {tstddev:4.1}MiBps");
-            }
+        let mut scores: Vec<_> = scores.into_iter().collect();
+        scores.sort_by(|x, y| {
+            (x.1.ratio.mean(), x.1.speed.mean())
+                .cmp(&(y.1.ratio.mean(), y.1.speed.mean()))
+        });
+        for (_, score) in scores.iter() {
+            let zname = format!("{:?}", score.compressor);
+            let shufname = score.shuffle;
+            let zmin = score.ratio.minimum().unwrap() as f64 / 10.0;
+            let zmean = score.ratio.mean().unwrap() as f64 / 10.0;
+            let zmax = score.ratio.maximum().unwrap() as f64 / 10.0;
+            let zstddev = score.ratio.stddev().unwrap() as f64 / 10.0;
+            let tmean = score.speed.mean().unwrap();
+            let tstddev = score.speed.stddev().unwrap();
+            print!("{ds:10}{zname:15}{shufname:8}| {zmin:5.1}% {zmean:5.1}% {zmax:5.1}% {zstddev:5.1}%");
+            println!(" | {tmean:5.1} MiBps {tstddev:4.1} MiBps");
         }
     }
 }
